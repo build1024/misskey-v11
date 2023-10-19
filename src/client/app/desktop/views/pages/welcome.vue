9f295eb8cdd3bf8edebe032567731e62424255a0
hira <1009497+build1024@users.noreply.github.com>
2023-10-20 00:08:49 +0900

    Simplify welcome page layout

--- src/client/app/desktop/views/pages/welcome.vue
+++ src/client/app/desktop/views/pages/welcome.vue
@@ -35,7 +35,7 @@
 						<span class="signin" @click="signin">{{ $t('@.signin') }}</span>
 					</p>
 
-					<img v-if="meta" :src="meta.mascotImageUrl" alt="" title="藍" class="char">
+					<img v-if="meta" :src="meta.mascotImageUrl" alt="" title="front image" class="char">
 				</div>
 			</div>
 
@@ -57,12 +57,6 @@
 				</div>
 			</div>
 
-			<div class="tag-cloud block">
-				<div>
-					<mk-tag-cloud/>
-				</div>
-			</div>
-
 			<div class="nav block">
 				<div>
 					<mk-nav class="nav"/>
@@ -70,12 +64,6 @@
 			</div>
 
 			<div class="side">
-				<div class="trends block">
-					<div>
-						<mk-trends/>
-					</div>
-				</div>
-
 				<div class="tl block">
 					<header><fa :icon="['far', 'comment-alt']"/> {{ $t('timeline') }}</header>
 					<div>
@@ -364,10 +352,10 @@ export default Vue.extend({
 
 		> .body
 			display grid
-			grid-template-rows 390px 1fr 256px 64px
+			grid-template-rows 390px 1fr 64px
 			grid-template-columns 1fr 1fr 350px
 			gap 16px
-			height 1150px
+			height 878px
 
 			> .main
 				grid-row 1
@@ -460,27 +448,19 @@ export default Vue.extend({
 						background-position center center
 						background-size cover
 
-			> .tag-cloud
-				grid-row 3
-				grid-column 1 / 3
-
-				> div
-					height 256px
-					padding 32px
-
 			> .nav
 				display flex
 				justify-content center
 				align-items center
-				grid-row 4
+				grid-row 3
 				grid-column 1 / 3
 				font-size 14px
 
 			> .side
 				display grid
-				grid-row 1 / 5
+				grid-row 1 / 4
 				grid-column 3
-				grid-template-rows 1fr 350px
+				grid-template-rows 1fr 125px
 				grid-template-columns 1fr
 				gap 16px
 
@@ -489,13 +469,8 @@ export default Vue.extend({
 					grid-column 1
 					overflow auto
 
-				> .trends
-					grid-row 2
-					grid-column 1
-					padding 8px
-
 				> .info
-					grid-row 3
+					grid-row 2
 					grid-column 1
 
 					> div
